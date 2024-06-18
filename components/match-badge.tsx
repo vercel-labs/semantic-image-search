3f23e96c862aafe2a56d6671e46ff438aaee69ab
Nico Albanese <nico.albanese@vercel.com>
2024-06-19 00:24:25 +0100

    fix: badge copy on mobile

--- components/match-badge.tsx
+++ components/match-badge.tsx
@@ -10,9 +10,20 @@ export const MatchBadge = ({
   return (
     <div className="">
       {type === "semantic" ? (
-        <Badge variant={"default"} className="bg-green-100 text-green-700">
-          Semantic Match: {similarity?.toFixed(3)}
-        </Badge>
+        <>
+          <Badge
+            variant={"default"}
+            className="block sm:hidden bg-green-100 text-green-700"
+          >
+            Similarity: {similarity?.toFixed(3)}
+          </Badge>
+          <Badge
+            variant={"default"}
+            className="hidden sm:block bg-green-100 text-green-700"
+          >
+            Semantic Match: {similarity?.toFixed(3)}
+          </Badge>
+        </>
       ) : (
         <Badge variant={"secondary"}>Direct Match</Badge>
       )}
