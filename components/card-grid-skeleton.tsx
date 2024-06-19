bc16e9ac7051a20a9a6d7acc034a048978ca52f6
Nico Albanese <nico.albanese@vercel.com>
2024-06-19 10:12:37 +0100

    fix: skeleton size

--- components/card-grid-skeleton.tsx
+++ components/card-grid-skeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export function CardGridSkeleton() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-2">
+    <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-2">
       {new Array(16).fill("").map((_, i) => (
         <SkeletonCard key={i} />
       ))}
@@ -13,7 +13,7 @@ export function CardGridSkeleton() {
 export function SkeletonCard() {
   return (
     <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[450px] min-w-48 rounded-xl" />
+      <Skeleton className="h-[250px] sm:h-[450px] rounded-xl" />
     </div>
   );
 }
